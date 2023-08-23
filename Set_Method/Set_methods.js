c1856201985aede44a6174f1c2a20b1ce344a427
smitsavaliya321@gmail.com <smitsavaliya321@gmail.com>
2023-08-23 05:04:50 -0700

    set methods in javascript

---
+++ Set_Method/Set_methods.js
@@ -0,0 +1,62 @@
+//set methods
+
+/*
+set methods : -
+new set
+add() Adds a new element to the Set
+delete() Remove an element from a Set
+has() Returns true if a value exists in the Set
+clear() Removes all elements from a Set
+forEach() Invokes a callback for each element in the Set
+entries() Returns an iterator with all the values in a Set
+size Returns a number of elements in a Set
+*/
+
+let data =new Set([
+    'surat' , 'ahamdabad' , 'jasadan' , 'Rajkot' , 'Randhiya'
+]);
+
+// console.log(data);
+
+//add
+// console.log(data.add("vesu"));
+// console.log(data);
+
+//delete
+// console.log(data.delete("Rajkot"));
+// console.log(data);
+
+//has
+// console.log(data.has("surat"));
+// console.log(data);
+
+//clear
+// console.log(data.clear());
+// console.log(data);
+
+//entries
+// console.log(data.entries());
+// console.log(data);
+
+//size
+// console.log(data.size);
+
+//foreach
+
+// let a = " ";
+
+// data.forEach(function(val) {
+    
+//     a += val + "\n" ;
+// }) 
+// console.log(a);
+
+
+//intilization:-
+
+// const hii =new Set();
+// hii.add(10);
+// hii.add(20);
+// hii.add(30);
+// hii.add(40);
+// console.log(hii);
