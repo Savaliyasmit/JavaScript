87a6e21a15fbc1fd6712026319c6ea3401937d0e
smitsavaliya321@gmail.com <smitsavaliya321@gmail.com>
2023-08-24 01:20:24 -0700

    String literls in javascript

---
+++ Strings_Literlas/Literlas.js
@@ -0,0 +1,29 @@
+//String Literls
+
+//ES 5 
+// console.log('string is ' + 'string');
+
+//ES 6use backtick => `you have entre "doblequtos "  `
+// console.log(`my name is smit.
+// my name age is "19".`);
+
+//ES5 b  merge two variables string
+// let f = 'smit'
+// let l = 'savaliya'
+// console.log("my first name is " + f + "." , "my last name is " + l + ".");
+
+//ES6 after merge two string short method use backtick = '' and ${expresion}
+// let f = 'smit'
+// let l = 'savaliya'
+// console.log(`my name is ${f}.
+// my last name is ${l}.`);
+
+//ES5 before we can add expression too ${}
+// let a = 50 ;
+// let b = 50 ;
+// console.log("answer is  :-" + (a+b) +".")
+
+//ES6 after 
+// let a = 50 ;
+// let b = 50 ;
+// console.log(`Answer is :- ${a+b} .`);
