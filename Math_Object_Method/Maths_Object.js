8290096653d70f7a8cfc86489c4c55eb70f7ffac
smitsavaliya321@gmail.com <smitsavaliya321@gmail.com>
2023-08-28 00:15:51 -0700

    Maths Methods

---
+++ Math_Object_Method/Maths_Object.js
@@ -0,0 +1,21 @@
+//Maths Object
+/*
+Math.E // returns Euler's number 
+Math.PI // returns PI  (main)
+Math.SQRT2 // returns the square root of 2
+Math.SQRT1_2 // returns the square root of 1/2
+Math.LN2 // returns the natural logarithm of 2
+Math.LN10 // returns the natural logarithm of 10
+Math.LOG2E // returns base 2 logarithm of E
+Math.LOG10E // returns base 10 logarithm of E
+*/
+//valu is fix maths
+// console.log(Math.E); //Math E  
+// console.log(Math.PI); //pie 3.14
+// console.log(Math.SQRT2); //squre root of 2
+// console.log(Math.SQRT1_2);//squre root of 2 and half
+// console.log(Math.LN2);
+// console.log(Math.LOG2E);
+// console.log(Math.LN10);
+// console.log(Math.LOG10E);
+
