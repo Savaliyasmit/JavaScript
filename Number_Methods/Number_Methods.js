fd0d3217cb1ff686206ad11100d23ceeef870efb
smitsavaliya321@gmail.com <smitsavaliya321@gmail.com>
2023-08-29 15:35:38 +0530

    Number Methods

---
+++ Number_Methods/Number_Methods.js
@@ -0,0 +1,28 @@
+/*
+imp :-toString()	Returns a number as a string
+toExponential()	Returns a number written in exponential notation
+toFixed()	Returns a number written with a number of decimals
+toPrecision()	Returns a number written with a specified length
+ValueOf()	Returns a number as a number
+*/
+
+//1.toString() covert number to string
+// console.log((11).toString());
+//  console.log((11+11).toString());
+
+//2.toExponential()  4 to mens point after four value 1.2500 
+// console.log((125).toExponential(4));
+// console.log((125).toExponential());
+// console.log((12558575).toExponential(5));   5 and 5 to up +1
+
+//toPrecision() 
+// console.log((1.23636641212386).toPrecision());
+// console.log((1.2584595).toPrecision());
+
+//toFixed() returns a number written with a number of decimals
+//console.log((1.254678).toFixed(4));  fix to after point of value
+//console.log((123456.2546).toFixed());  not give fix value print point before
+
+//ValueOf()	Returns a number as a number
+// console.log((1.2524095075).valueOf()); give as value as.
+
