31d1b567772c4a1a1f9ba79aae080b6b4acebe5c
smitsavaliya321@gmail.com <smitsavaliya321@gmail.com>
2023-09-07 10:40:38 +0530

    Maths Methods

---
+++ Math_Object_Method/Maths_Methods.js
@@ -0,0 +1,48 @@
+//Maths Methods
+
+//ceil round up
+// console.log(Math.ceil(4.1));// round up
+// console.log(Math.ceil(-3.0));// round up
+  
+//floor round down
+// console.log(Math.floor(5.9));
+// console.log(Math.floor(-4.9));
+
+//round  round figure a value 
+//  console.log(Math.round(4.9));
+// console.log(Math.round(-4.9));
+
+//SQRT squre of root number
+// console.log(Math.sqrt(25));
+
+//pow means power 2*2*2*2*2*2 =64
+// console.log(Math.pow(2,6));
+
+//abs mens absuolte  value they never show positive ore negative always print posotive number
+//  console.log(Math.abs(-5));
+//  console.log(Math.abs(6));
+
+//random mens random value generate
+// console.log(Math.random()*1000);//0 to 1000 genereate random 
+// console.log(Math.random());//0 to 1 number generate
+
+//trunc return always interger any case
+// console.log(Math.trunc(14.6));
+
+//min and max
+// console.log(Math.min(15,67,30));
+// console.log(Math.max(15,67,30));
+
+//log,log2,log10
+// console.log(Math.log(10));
+// console.log(Math.log2(10));
+// console.log(Math.log10(10));
+
+//sin,cos,tan found table value 
+// console.log(Math.sign(67)); //return positive and nagative 1 and 0
+// console.log(Math.sin(90 *Math.PI/180));
+// console.log(Math.cos(60 *Math.PI/180));
+// console.log(Math.tan(45 *Math.PI/180));
+// console.log(Math.tanh(90 *Math.PI/180));
+// console.log(Math.cosh(90 *Math.PI/180));
+// console.log(Math.sinh(90 *Math.PI/180));
