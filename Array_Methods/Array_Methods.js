2c129c22f6053a17a9da39df1d41ec714bc3179f
smitsavaliya321@gmail.com <smitsavaliya321@gmail.com>
2023-09-11 17:15:42 +0530

    Array Methods

---
+++ Array_Methods/Array_Methods.js
@@ -0,0 +1,82 @@
+/*
+length
+1.push
+2.pop
+3.unshift
+4.shift
+5.concat
+6.sort
+7.reverse
+
+*/
+
+//all types of data type allow in array like number,float value,string,bollean
+//  let array =  [1,2,55.55,'smit','savaliya',true,false,null] ;
+// console.log(array);
+
+//diffrent way to add index to araay
+// let index = []
+// index[0]=1
+// index[1]=2
+// index[2]=4
+// index[3]=true
+// console.log(index);
+
+//calculate array length
+// array [99] = 40
+// console.log(array.length);
+// console.log(array);
+
+//undefined
+// let reult = array[30]
+// console.log(reult);
+
+//conver to stringe
+// let reult = array.toString()
+// console.log(reult);
+
+//1.push ->push element in array (last)
+// let arry1 = [0,1,2,3,4,5,6]
+// arry1.push(7,8,9,10,11,12,13,14)
+// console.log(arry1);
+
+//2.pop ->pop delete element in array (last)
+// let arry2 = [0,1,2,3,4,5,6]
+
+// arry2.pop()
+// arry2.pop()
+// arry2.pop()
+// arry2.pop()
+// arry2.pop()
+// console.log(arry2);
+
+//3.shift entre  element in array (first)
+// let arry3 = [1,3,35,35,6,6,6]
+// arry3.unshift(0,-1,-2,-3)
+// console.log(arry3);
+
+
+// //4.unshift delete  element in array (first)
+// let arry4 = [1,3,35,5,6,6,6]
+// arry4.shift()
+// arry4.shift()
+// arry4.shift()
+// console.log(arry4);
+
+//5.merge two array
+// let a1 = [3,46,5,57,]
+// let a2 = [58,59,60]
+// let a3 = [true,false]
+
+// let reult = a1.concat(a2,a3)
+// console.log(reult);
+
+//6.sort accending order array 
+// let arry5 = [1,34,56,78,34,22,0]
+// arry5.sort()
+// console.log(arry5);
+
+//7reverse use to array element reverse
+// let arry6 = [5,4,32,5,2,535,33,0]
+// arry6.reverse()
+// console.log(arry6);
