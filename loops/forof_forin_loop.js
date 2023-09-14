80f492b34499d71e52a87c269a83310bd7a02c54
smitsavaliya321@gmail.com <smitsavaliya321@gmail.com>
2023-09-14 15:12:09 +0530

    forof ann forin loops

---
+++ loops/forof_forin_loop.js
@@ -0,0 +1,47 @@
+//for in loop use to most in object itterate otherwise use in array and string
+// const per ={
+//     pertrol2018 : "10%",
+//     pertrol2019 : "20%",
+//     pertrol2020 : "100%",
+    
+// }
+// console.log(per.pertrol2018);
+
+//this is count object length
+// console.log(Object.keys(per).length);
+
+//1.object
+// for(let l in per){
+//     console.log(per[l]);
+// }
+
+//2.array
+// const arry = [232,2,32,3,323,23,232]
+// for(let y in arry){
+//     console.log(arry[y]);
+// }
+
+//3.string
+// const str = "smit savaliya"
+// for(let s in str){
+//     console.log(str[s]);
+    
+// }
+
+
+
+//for of loop use in array and string not use in object
+
+//1.array
+// const array = [23,2,3,23,23,23]
+
+// for(let a of array){
+//     console.log(a);
+// }
+
+//2.strings
+// const str = "smit savaliya"
+
+// for(let s of str){
+//     console.log(s);
+// }
